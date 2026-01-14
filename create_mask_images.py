013a0e32053f2855694d490e89ac96f47f40089b
osalamon <ondrej.salamon@vsb.cz>
2026-01-14 14:50:58 +0100

    WIP: rewriting creation of training data to take into account all single objects

--- create_mask_images.py
+++ create_mask_images.py
@@ -1,4 +1,4 @@
-import tifffile as tiff
+import tifffile as tf
 import numpy as np
 import os
 
@@ -17,7 +17,7 @@ def create_train_data(input_path, gt_path):
         raise ValueError(f"No .tif files found in {gt_path}")
     # Read the first image to get the TRUE shape
     first_image_path = os.path.join(gt_path, valid_files[0])
-    sample_img = tiff.imread(first_image_path)
+    sample_img = tf.imread(first_image_path)
     true_shape = sample_img.shape  # This will likely be (1036, 1070)
     print(f"Detected image shape: {true_shape}")
 
@@ -29,25 +29,40 @@ def create_train_data(input_path, gt_path):
     if num_files == 0:
         raise ValueError(f"No .tif files found in {gt_path}")
 
-    # 2. Dynamically allocate memory based on what you actually have
-    mask_imgs = np.zeros((num_files, true_shape[0], true_shape[1]), dtype='float32')
+    ## Dynamically allocate memory based on what you actually have
+    ## mask_imgs = np.zeros((num_files, true_shape[0], true_shape[1]), dtype='float32')
 
+    # 2. init of list for collecting processed sub-images (i.e. single object - cell)
+    objects_subimages = []
+
+    # 3. Firstly, I define width and height STATICALLY for cropping sub-images around each object
+    crop_h, crop_w = 101, 101
+
+    # 4. Outer loop: process each file/image
     ii = 0
     print('Loading masks from ', input_path)
     for image_name in os.listdir(gt_path):  # Go through all the .tif files in the folder
         if image_name.endswith(".tif"):
             # Extract the number safely (removes '.tif' first so we don't accidentally slice it)
-            # This handles 'man_seg090.tif' -> '090' OR 'man_seg1000.tif' -> '1000'
             image_id = image_name.split('man_seg')[1].split('.')[0]
             # Construct the filename. 
             # I need 4 digits, i.e. 'mask0090.tif', use .zfill(4)
             participant_image_name = 'mask' + image_id.zfill(4) + '.tif'
             # participant_image_name = 'mask' + image_name.split('man_seg')[1][:3] + '.tif'
-            im_gt = tiff.imread(os.path.join(gt_path, image_name))
-            im_allmasks = tiff.imread(os.path.join(input_path, participant_image_name))
+            im_gt = tf.imread(os.path.join(gt_path, image_name))
+            im_allmasks = tf.imread(os.path.join(input_path, participant_image_name))
+            
+            cur_labels = np.unique(im_gt)
+            cur_labels = cur_labels[cur_labels != 0]     # Remove 0 (background) if present # TODO not sure about this line
+
+            # 5. Inner Loop: Iterate through unique object labels found within that file/image
+            for cur_label in cur_labels:
+                
+            
             # mask_im = np.zeros((im_allmasks.shape[0], im_allmasks.shape[1]), dtype='float32')
-            mask_im = np.zeros((im_allmasks.shape[0], im_allmasks.shape[1]), dtype='float32')
-            cur_label = np.unique(im_gt)[-1]
+            #mask_im = np.zeros((im_allmasks.shape[0], im_allmasks.shape[1]), dtype='float32')
+            #cur_label = np.unique(im_gt)[-1]
+            
             mask_im[im_allmasks == cur_label] = 1.0	# to produce a binary image for each label since DeepFuse is a 2-class classifier
             mask_imgs[ii, :, :] = mask_im
             ii = ii+1
@@ -71,7 +86,7 @@ def create_gt_data(gt_path):
         raise ValueError(f"No .tif files found in {gt_path}")
     # Read the first image to get the TRUE shape
     first_image_path = os.path.join(gt_path, valid_files[0])
-    sample_img = tiff.imread(first_image_path)
+    sample_img = tf.imread(first_image_path)
     true_shape = sample_img.shape  # This will likely be (1036, 1070)
     print(f"Detected image shape: {true_shape}")
 
@@ -91,7 +106,7 @@ def create_gt_data(gt_path):
     for image_name in os.listdir(gt_path):  # Go through all the .tif files in the folder
         if image_name.endswith(".tif"):
             print('Reading image', image_name)
-            im_allmasks = tiff.imread(os.path.join(gt_path, image_name))
+            im_allmasks = tf.imread(os.path.join(gt_path, image_name))
             im_allmasks [im_allmasks > 0.5] = 1.0 # to produce a binary image
             im_allmasks [im_allmasks < 0.5] = 0.0
             gt_mask_imgs[ii, :, :] = im_allmasks
